4c91f491cebec1894f03e4fd2fbd4e1139d5c9ca
unknown <adrianforero@colegioguanenta.edu.co>
2023-06-19 21:43:23 -0500

    FUNCIONA

--- player.py
+++ player.py
@@ -6,8 +6,12 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32,64))
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft=pos)
+        
+        # player movement
         self.direction = pygame.math.Vector2(0,0)
         self.speed = 8
+        self.gravity = 0.8
+        self.jump_speed = -16
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -18,6 +22,18 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
+        if keys[pygame.K_SPACE]:
+            self.jump()
+
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+
+    def jump(self):
+        self.direction.y = self.jump_speed
+        keys = pygame.key.get_pressed()
+
     def update(self):
         self.get_input()
         self.rect.x += self.direction.x * self.speed
+        self.apply_gravity()
