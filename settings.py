1eecc59a7d09ce999c495c49a725335ef88175d5
unknown <adrianforero@colegioguanenta.edu.co>
2023-06-19 20:02:49 -0500

    doing the game

---
+++ settings.py
@@ -0,0 +1,18 @@
+level_map = [
+'                            ',
+'                            ',
+'      XXXXXXX               ',
+'        XXX     XXX         ',
+' X P                        ',
+' XXXX                 XXXXX ',
+' XXXXX                XXXX  ',
+'                       XX   ',
+'                            ',
+'          XXXX              ',
+'    XXX          XXX        ',
+'XXXXXXX                 XXXX',
+'XXXXXXX                 XXXX']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map)*tile_size
